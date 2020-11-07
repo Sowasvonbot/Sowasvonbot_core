96cf966a94b78e93762f85624ffb5502919cd104
Tobias <tms.kleinert@gmx.de>
2020-11-07 22:05:57 +0100

    Upgraded JDA

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module main {
-    requires net.dv8tion.jda;
-    requires okhttp3;
-    requires slf4j.api;
-    requires jsr305;
-    requires org.apache.logging.log4j.core;
-    requires java.net.http;
-    requires org.json;
-}
